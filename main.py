10573c3b9039ab34f0f729a762bd6bd4b93db141
Guilherme <rodriguesgui48@gmail.com>
2025-03-14 00:48:49 -0300

    Dados de acesso ao banco de dados colocados dentro do .env

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ TOKEN = os.getenv("TOKEN")
 
 try:
     conexao = psycopg2.connect(
-        host='localhost',
-        port=5432,
-        dbname='postgres',
-        user='postgres',
-        password='macaco',
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
         
     )
     
